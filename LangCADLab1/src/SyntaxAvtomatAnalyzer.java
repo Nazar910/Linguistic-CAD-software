4ee8808a69833784bafc551985bdbaf92fd4afc9
Nazar910 <pyvovarnazar@gmail.com>
2016-11-21 07:55:46 +0200

    modified SyntaxAvtomatAnalyzer

--- LangCADLab1/src/SyntaxAvtomatAnalyzer.java
+++ LangCADLab1/src/SyntaxAvtomatAnalyzer.java
@@ -60,59 +60,59 @@ public class SyntaxAvtomatAnalyzer {
     static {
         stack = new Stack();
         avtomatList.add(null);
-        avtomatList.add(new Record(new int[]{1}, new int[]{2}, new String[]{""}, "", "error: Porgram must start with prog"));//1 prog
-        avtomatList.add(new Record(new int[]{28}, new int[]{3}, new String[]{""}, "", "error"));//2 idn
-        avtomatList.add(new Record(new int[]{11}, new int[]{4}, new String[]{""}, "", "error"));//3 ⁋
-        avtomatList.add(new Record(new int[]{2}, new int[]{5}, new String[]{""}, "", "error"));//4 var
-        avtomatList.add(new Record(new int[]{3, 4}, new int[]{6, 6}, new String[]{"", ""}, "", "error"));//5 int real
-        avtomatList.add(new Record(new int[]{28}, new int[]{7}, new String[]{""}, "", "error"));//6 idn
-        avtomatList.add(new Record(new int[]{12, 11}, new int[]{6, 8}, new String[]{"", ""}, "", "error"));//7 , ⁋
-        avtomatList.add(new Record(new int[]{3, 4, 9}, new int[]{6, 6, 11}, new String[]{"", "", "push9"}, "", "error"));//8 int real <op>
+        avtomatList.add(new Record(new int[]{1}, new int[]{2}, new String[]{""}, "", "error: Program have to start with prog"));//1 prog
+        avtomatList.add(new Record(new int[]{28}, new int[]{3}, new String[]{""}, "", "error: After \"prog\" must be idn"));//2 idn
+        avtomatList.add(new Record(new int[]{11}, new int[]{4}, new String[]{""}, "", "error: \"⁋\" expected"));//3 ⁋
+        avtomatList.add(new Record(new int[]{2}, new int[]{5}, new String[]{""}, "", "error: Expected var"));//4 var
+        avtomatList.add(new Record(new int[]{3, 4}, new int[]{6, 6}, new String[]{"", ""}, "", "error: Type have to be int or real"));//5 int real
+        avtomatList.add(new Record(new int[]{28}, new int[]{7}, new String[]{""}, "", "error: Expected idn"));//6 idn
+        avtomatList.add(new Record(new int[]{12, 11}, new int[]{6, 8}, new String[]{"", ""}, "", "error: \"⁋\" expected"));//7 , ⁋
+        avtomatList.add(new Record(new int[]{3, 4, 9}, new int[]{6, 6, 11}, new String[]{"", "", "push9"}, "", ""));//8 int real <op>
         avtomatList.add(new Record(new int[]{11}, new int[]{10}, new String[]{""}, "", "error: Operators have to be divided by \"⁋\""));//9 ⁋
         avtomatList.add(new Record(new int[]{10, 0}, new int[]{0, 11}, new String[]{"", "push9"}, "exit", ""));//10 }
-        avtomatList.add(new Record(new int[]{5, 6, 28, 8, 7, 17}, new int[]{12, 15, 45, 18, 31, 49}, new String[]{"", "", "push51", "", "", ""}, "", "error"));//11 cout cin idn for if (
+        avtomatList.add(new Record(new int[]{5, 6, 28, 8, 7, 17}, new int[]{12, 15, 45, 18, 31, 49}, new String[]{"", "", "push51", "", "", ""}, "", "error: Operator have to be cout,cin,for,if,assignment or ternary operator"));//11 cout cin idn for if (
         avtomatList.add(new Record(new int[]{20}, new int[]{13}, new String[]{""}, "", "error: Operands of cout have to be divided by \"<<\""));//12 <<
         avtomatList.add(new Record(new int[]{28, 29}, new int[]{14, 14}, new String[]{"", ""}, "", "error: Operands of cout must be idn or con"));//13 idn con
         avtomatList.add(new Record(new int[]{20}, new int[]{13}, new String[]{""}, "", "exit"));//14 <<
-        avtomatList.add(new Record(new int[]{21}, new int[]{16}, new String[]{""}, "", "error"));//15 >>
-        avtomatList.add(new Record(new int[]{28}, new int[]{17}, new String[]{""}, "", "error"));//16 idn
+        avtomatList.add(new Record(new int[]{21}, new int[]{16}, new String[]{""}, "", "error: Operators of cin have to be divided by \">>\""));//15 >>
+        avtomatList.add(new Record(new int[]{28}, new int[]{17}, new String[]{""}, "", "error: Expecting idn in cin"));//16 idn
         avtomatList.add(new Record(new int[]{21}, new int[]{16}, new String[]{""}, "", "exit"));//17 >>
-        avtomatList.add(new Record(new int[]{17}, new int[]{19}, new String[]{""}, "", "error"));//18 (
-        avtomatList.add(new Record(new int[]{28}, new int[]{20}, new String[]{""}, "", "error"));//19 idn
-        avtomatList.add(new Record(new int[]{19}, new int[]{21}, new String[]{""}, "", "error"));//20 =
-        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push22"}, "", "error"));//21 <E> push22
-        avtomatList.add(new Record(new int[]{35}, new int[]{23}, new String[]{""}, "", "error"));//22 ;
-        avtomatList.add(new Record(new int[]{0}, new int[]{42}, new String[]{"push24"}, "", "error"));//23 <LE> push24
-        avtomatList.add(new Record(new int[]{35}, new int[]{25}, new String[]{""}, "", "error"));//24 ;
-        avtomatList.add(new Record(new int[]{28}, new int[]{26}, new String[]{""}, "", "error"));//25 idn
-        avtomatList.add(new Record(new int[]{19}, new int[]{27}, new String[]{""}, "", "error"));//26 =
-        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push28"}, "", "error"));//27 <E> push28
-        avtomatList.add(new Record(new int[]{18}, new int[]{29}, new String[]{""}, "", "error"));//28 )
+        avtomatList.add(new Record(new int[]{17}, new int[]{19}, new String[]{""}, "", "error: Expecting \"(\" after \"for\""));//18 (
+        avtomatList.add(new Record(new int[]{28}, new int[]{20}, new String[]{""}, "", "error: Expecting idn"));//19 idn
+        avtomatList.add(new Record(new int[]{19}, new int[]{21}, new String[]{""}, "", "error: Expecting \"=\""));//20 =
+        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push22"}, "", ""));//21 <E> push22
+        avtomatList.add(new Record(new int[]{35}, new int[]{23}, new String[]{""}, "", "error: Expecting \";\""));//22 ;
+        avtomatList.add(new Record(new int[]{0}, new int[]{42}, new String[]{"push24"}, "", ""));//23 <LE> push24
+        avtomatList.add(new Record(new int[]{35}, new int[]{25}, new String[]{""}, "", "error: Expecting \";\""));//24 ;
+        avtomatList.add(new Record(new int[]{28}, new int[]{26}, new String[]{""}, "", "error: Expecting idn"));//25 idn
+        avtomatList.add(new Record(new int[]{19}, new int[]{27}, new String[]{""}, "", "error: Expecting \"=\""));//26 =
+        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push28"}, "", ""));//27 <E> push28
+        avtomatList.add(new Record(new int[]{18}, new int[]{29}, new String[]{""}, "", "error: Expecting \")\""));//28 )
         avtomatList.add(new Record(new int[]{0}, new int[]{11}, new String[]{"push30"}, "", ""));//29 <op> push30
         avtomatList.add(new Record(new int[]{0}, new int[]{0}, new String[]{""}, "", "exit"));//30
-        avtomatList.add(new Record(new int[]{17}, new int[]{32}, new String[]{""}, "", "error"));//31 (
-        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push33"}, "", "error"));//32 <E> push33
-        avtomatList.add(new Record(new int[]{23, 25, 22, 24, 26, 27}, new int[]{34, 34, 34, 34, 34, 34}, new String[]{"", "", "", "", "", ""}, "", "error"));//33 > >= < <= == !=
-        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push35"}, "", "error"));//34 <E> push35
-        avtomatList.add(new Record(new int[]{18}, new int[]{36}, new String[]{""}, "", "error"));//35 )
-        avtomatList.add(new Record(new int[]{9}, new int[]{11}, new String[]{"push37"}, "", "error"));//36 { <op> push37
-        avtomatList.add(new Record(new int[]{11}, new int[]{38}, new String[]{""}, "", "error"));//37 ⁋
+        avtomatList.add(new Record(new int[]{17}, new int[]{32}, new String[]{""}, "", "error: Expecting \"(\" after \"if\""));//31 (
+        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push33"}, "", ""));//32 <E> push33
+        avtomatList.add(new Record(new int[]{23, 25, 22, 24, 26, 27}, new int[]{34, 34, 34, 34, 34, 34}, new String[]{"", "", "", "", "", ""}, "", "error: Expecting reference sign"));//33 > >= < <= == !=
+        avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push35"}, "", ""));//34 <E> push35
+        avtomatList.add(new Record(new int[]{18}, new int[]{36}, new String[]{""}, "", "error: Expecting \")\""));//35 )
+        avtomatList.add(new Record(new int[]{9}, new int[]{11}, new String[]{"push37"}, "", ""));//36 { <op> push37
+        avtomatList.add(new Record(new int[]{11}, new int[]{38}, new String[]{""}, "", "error: Operators have to be divided by \"⁋\""));//37 ⁋
         avtomatList.add(new Record(new int[]{10, 0}, new int[]{0, 11}, new String[]{"", "push37"}, "exit", ""));//38 }
         //**<E>****//
-        avtomatList.add(new Record(new int[]{28, 29, 17}, new int[]{40, 40, 39}, new String[]{"", "", "push41"}, "", "error"));//39 idn con (
+        avtomatList.add(new Record(new int[]{28, 29, 17}, new int[]{40, 40, 39}, new String[]{"", "", "push41"}, "", "error: Expecting idn con or \"(\""));//39 idn con (
         avtomatList.add(new Record(new int[]{13, 14, 15, 16}, new int[]{39, 39, 39, 39}, new String[]{"", "", "", ""}, "", "exit"));//40 +-*/ exit
-        avtomatList.add(new Record(new int[]{18}, new int[]{40}, new String[]{""}, "", "error"));//41 )
+        avtomatList.add(new Record(new int[]{18}, new int[]{40}, new String[]{""}, "", "error: Missing closing bracket in expression"));//41 )
         //**<LE>***//
         avtomatList.add(new Record(new int[]{32, 0}, new int[]{42, 39}, new String[]{"", "push43"}, "", ""));//42 not
-        avtomatList.add(new Record(new int[]{23, 25, 22, 24, 26, 27}, new int[]{39, 39, 39, 39, 39, 39}, new String[]{"push44", "push44", "push44", "push44", "push44", "push44"}, "", "error"));//43 > >= < <= == !=
+        avtomatList.add(new Record(new int[]{23, 25, 22, 24, 26, 27}, new int[]{39, 39, 39, 39, 39, 39}, new String[]{"push44", "push44", "push44", "push44", "push44", "push44"}, "", "error: Missing reference sign"));//43 > >= < <= == !=
         avtomatList.add(new Record(new int[]{30, 31}, new int[]{42, 42}, new String[]{"", ""}, "", "exit"));//44 and or
         //**idn = **//
-        avtomatList.add(new Record(new int[]{19}, new int[]{46}, new String[]{""}, "", "error"));//45 =
+        avtomatList.add(new Record(new int[]{19}, new int[]{46}, new String[]{""}, "", "error: Expecting \"=\""));//45 =
         avtomatList.add(new Record(new int[]{0}, new int[]{39}, new String[]{"push47"}, "", ""));//46 <E>
         avtomatList.add(new Record(new int[]{23, 25, 22, 24, 26, 27}, new int[]{39, 39, 39, 39, 39, 39}, new String[]{"push48", "push48", "push48", "push48", "push48", "push48"}, "", "exit"));//47 exit < <= > >= == !=
         avtomatList.add(new Record(new int[]{30, 31, 0}, new int[]{46, 46, 49}, new String[]{"", "", "pop51"}, "", "exit"));//48 and or
-        avtomatList.add(new Record(new int[]{33}, new int[]{39}, new String[]{"push50"}, "", "error"));//49 ?
-        avtomatList.add(new Record(new int[]{34}, new int[]{39}, new String[]{"push51"}, "", "error"));//50 :
+        avtomatList.add(new Record(new int[]{33}, new int[]{39}, new String[]{"push50"}, "", "error: Expecting \"?\""));//49 ?
+        avtomatList.add(new Record(new int[]{34}, new int[]{39}, new String[]{"push51"}, "", "error: Expecting \":\""));//50 :
         avtomatList.add(new Record(new int[]{0}, new int[]{0}, new String[]{""}, "", "exit"));//51 exit
     }
 
