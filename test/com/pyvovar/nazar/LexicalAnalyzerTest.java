eb869de02cd6053af9ad779fcac6a47ca676b1f8
Nazar910 <pyvovarnazar@gmail.com>
2017-05-02 09:54:14 +0300

    remove verify

--- test/com/pyvovar/nazar/LexicalAnalyzerTest.java
+++ test/com/pyvovar/nazar/LexicalAnalyzerTest.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
